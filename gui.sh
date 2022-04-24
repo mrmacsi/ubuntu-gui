a738f7f22182e3fafafeded755432efd916e5d6d
MACIT SIMSEK <macitsimsek12@hotmail.com>
2022-04-24 21:28:49 +0100

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ls
+
 sudo apt-get update && sudo apt-get upgrade -y
 
 sudo apt-get install lightdm -y
@@ -10,8 +11,6 @@ sudo apt-get install tasksel -y
 sudo apt-get install ubuntu-desktop -y
 sudo systemctl set-default graphical.target
 
-sudo adduser macit
-
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -P /tmp
 sudo apt-get install /tmp/chrome-remote-desktop_current_amd64.deb -y
 
@@ -20,5 +19,7 @@ sudo apt-get install qemu-kvm -y
 sudo adduser $USER kvm
 sudo chown $USER /dev/kvm
 sudo chmod 777 -R /dev/kvm
+
+sudo adduser macit
 
 reboot
