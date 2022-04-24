de577ec4bffe612aed075e0c665e750903ff498e
MACIT SIMSEK <macitsimsek12@hotmail.com>
2022-04-24 20:37:28 +0100

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -15,8 +15,6 @@ sudo adduser macit
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -P /tmp
 sudo apt-get install /tmp/chrome-remote-desktop_current_amd64.deb -y
 
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AX4XfWi07qmQgnBSDlzxfQ3jcXxpWQtv0scnXYhwRsgumPkvOsY_12n_HVDMI9D9QW8g7A" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)  --user-name=macit
-
 sudo snap install android-studio --classic
 sudo apt-get install qemu-kvm -y
 sudo adduser $USER kvm
