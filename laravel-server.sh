be9eceee93fa7a0120858d508fb9bbb0fd90aaf0
MACIT SIMSEK <macitsimsek12@hotmail.com>
2022-12-24 11:06:55 +0000

    Update laravel-server.sh

--- laravel-server.sh
+++ laravel-server.sh
@@ -3,4 +3,5 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install php-mbstring php-xml php-bcmath php-curl
 sudo apt install mysql-server
+sudo systemctl start mysql.service
 sudo mysql
