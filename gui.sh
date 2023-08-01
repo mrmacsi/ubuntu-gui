5d1cac3f5bfdba34bd7959c80b3a09333ddd70d7
MACIT SIMSEK <macitsimsek12@hotmail.com>
2023-08-01 16:38:57 +0100

    Upgrade gui.sh

--- gui.sh
+++ gui.sh
@@ -1,27 +1,37 @@
 #!/bin/sh
 
-sudo apt-get update && sudo apt-get upgrade -y
+# Update and upgrade without asking questions
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -qq
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -qq
 
-sudo DEBIAN_FRONTEND="noninteractive" apt-get install lightdm -y
+# Install lightdm and start it
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq lightdm
 sudo systemctl start lightdm
 
-sudo apt-get install tasksel -y
+# Install tasksel
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq tasksel
 
-sudo apt-get install ubuntu-desktop -y
+# Install Ubuntu desktop and set graphical target as default
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq ubuntu-desktop
 sudo systemctl set-default graphical.target
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install ./google-chrome-stable_current_amd64.deb -y
+# Install Google Chrome
+wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq ./google-chrome-stable_current_amd64.deb
 
+# Add user 'macit' without asking for password and other details
 sudo adduser macit -c 'Macit' --disabled-password --gecos 'Macit,,,'
 
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo apt install ./chrome-remote-desktop_current_amd64.deb -y
+# Install Chrome Remote Desktop
+wget -q https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo DEBIAN_FRONTEND=noninteractive apt install -y -qq ./chrome-remote-desktop_current_amd64.deb
 
+# Create a swap file
 sudo fallocate -l 1G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 sudo cp /etc/fstab /etc/fstab.bak
 
+# Reboot the system
 sudo reboot
