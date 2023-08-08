0db5b2d2890f17e6b4b78029eca4a6dd4c67fcc5
MACIT SIMSEK <macitsimsek12@hotmail.com>
2023-08-09 00:27:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,50 +3,87 @@
 # Handle dpkg prompts non-interactively (keep local changes to configuration files)
 export DEBIAN_FRONTEND=noninteractive
 
-# Set up a Ubuntu desktop environment in the VM instance
-echo "Setting up Ubuntu desktop environment..."
+# --- Start setup ---
 
-# Refresh the repository and package lists, and perform necessary upgrades
-# Keeping any local changes to configuration files during the upgrade
+echo ""
+echo "------------------------------------"
+echo "Starting the setup..."
+echo ""
+
+# Refresh the repository and package lists
+echo ""
+echo "------------------------------------"
+echo "Refreshing repository and package lists..."
+echo ""
 sudo -E apt-get update
+
+# Perform necessary upgrades, keeping local changes to configuration files
+echo ""
+echo "------------------------------------"
+echo "Upgrading packages and keeping local changes to config files..."
+echo ""
 sudo DEBIAN_FRONTEND=noninteractive UCF_FORCE_CONFFOLD=1 apt-get -o Dpkg::Options::="--force-confold" --assume-yes -y upgrade
 
-# Install and set up SLiM Display Manager
+# Install SLiM Display Manager
+echo ""
+echo "------------------------------------"
+echo "Installing SLiM Display Manager..."
+echo ""
 echo "slim" | sudo DEBIAN_FRONTEND=noninteractive apt-get install -y slim
 
 # Install Ubuntu desktop environment
+echo ""
+echo "------------------------------------"
+echo "Installing Ubuntu desktop environment..."
+echo ""
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends lubuntu-desktop
 
-# Add user 'macit' without asking for password and other details
-sudo adduser macit --disabled-password --gecos 'Macit,,,'
+# Add user 'macit'
+echo ""
+echo "------------------------------------"
+echo "Adding user 'macit'..."
+echo ""
+sudo adduser macit --disabled-password --gecos 'Macit,,,,'
 
-# Set a default password for the user 'macit'
+# Set password for user 'macit'
+echo ""
+echo "------------------------------------"
+echo "Setting password for user 'macit'..."
+echo ""
 echo "macit:password" | sudo chpasswd
 
-# Create a 2GB swap file
+# Create swap file
+echo ""
+echo "------------------------------------"
+echo "Creating and configuring 2GB swap file..."
+echo ""
 sudo fallocate -l 2G /swapfile
-# Set the correct permissions for the swap file
 sudo chmod 600 /swapfile
-# Set up the swap space
 sudo mkswap /swapfile
-# Activate the swap file
 sudo swapon /swapfile
-# Backup the current fstab file
 sudo cp /etc/fstab /etc/fstab.bak
-# Add the swap file to fstab to make it permanent
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
-# Install Chrome Remote Desktop on the VM instance
+# Install Chrome Remote Desktop
+echo ""
+echo "------------------------------------"
 echo "Installing Chrome Remote Desktop..."
+echo ""
 sudo kill -9 $(lsof -t /var/lib/dpkg/lock-frontend)
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y xvfb xserver-xorg-video-dummy xbase-clients python3-packaging python3-psutil python3-xdg libgbm1
 sudo DEBIAN_FRONTEND=noninteractive dpkg -i chrome-remote-desktop_current_amd64.deb
 
-# Install Chromium on Ubuntu Desktop
-echo "Installing Chromium..."
+# Install Chromium browser
+echo ""
+echo "------------------------------------"
+echo "Installing Chromium browser..."
+echo ""
 sudo snap install chromium --classic
 
-# Reboot the machine
+# Reboot
+echo ""
+echo "------------------------------------"
 echo "Rebooting machine..."
+echo ""
 sudo reboot
