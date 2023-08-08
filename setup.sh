20512cfe31200c27ec421387656f41934ccfa0b8
MACIT SIMSEK <macitsimsek12@hotmail.com>
2023-08-08 18:22:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ sudo DEBIAN_FRONTEND=noninteractive UCF_FORCE_CONFFOLD=1 apt-get -o Dpkg::Option
 echo "slim" | sudo DEBIAN_FRONTEND=noninteractive apt-get install -y slim
 
 # Install Ubuntu desktop environment
-sudo apt-get install --no-install-recommends lxde
+sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends lxde
 
 # Add user 'macit' without asking for password and other details
 sudo adduser macit --disabled-password --gecos 'Macit,,,'
