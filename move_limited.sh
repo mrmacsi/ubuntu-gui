df28caaa471ee2c57023b2d686f8129b6f8bcd38
MACIT SIMSEK <macitsimsek12@hotmail.com>
2023-08-10 11:39:41 +0100

    Update move_limited.sh

--- move_limited.sh
+++ move_limited.sh
@@ -3,36 +3,43 @@
 # Log file path
 logfile="/home/macit/log.txt"
 
+# Ensure the log file exists
 if [ ! -f "$logfile" ]; then
     touch "$logfile"
 fi
 
-# Log the start time to the file
 echo "Script started at: $(date)" >> $logfile
+echo "Display set to: $DISPLAY" >> $logfile
 
 export DISPLAY=:20
 
+# Get the second occurrence of a window named "chromium"
 CHROMIUM_ID=$(xdotool search --name "chromium" | awk 'NR==2')
-echo $CHROMIUM_ID
+echo "Chromium Window ID: $CHROMIUM_ID" >> $logfile
+
+# NOTE: You assigned the ID to CHROMIUM_ID but were using $WINDOW_ID. This is an error in your script.
+# I've changed it to use $CHROMIUM_ID in the following lines:
 
 # Extracting X and Y positions of the Chromium window
-X_POS=$(xdotool getwindowgeometry $WINDOW_ID | grep Position | awk '{print $2}' | cut -d',' -f1)
-Y_POS=$(xdotool getwindowgeometry $WINDOW_ID | grep Position | awk '{print $2}' | cut -d',' -f2 | cut -d' ' -f1)
+X_POS=$(xdotool getwindowgeometry $CHROMIUM_ID | grep Position | awk '{print $2}' | cut -d',' -f1)
+Y_POS=$(xdotool getwindowgeometry $CHROMIUM_ID | grep Position | awk '{print $2}' | cut -d',' -f2 | cut -d' ' -f1)
+echo "Chromium Position: X=$X_POS, Y=$Y_POS" >> $logfile
 
 # Extracting window dimensions
-WINDOW_WIDTH=$(xdotool getwindowgeometry $WINDOW_ID | grep Geometry | awk '{print $2}' | cut -d'x' -f1)
-WINDOW_HEIGHT=$(xdotool getwindowgeometry $WINDOW_ID | grep Geometry | awk '{print $2}' | cut -d'x' -f2)
+WINDOW_WIDTH=$(xdotool getwindowgeometry $CHROMIUM_ID | grep Geometry | awk '{print $2}' | cut -d'x' -f1)
+WINDOW_HEIGHT=$(xdotool getwindowgeometry $CHROMIUM_ID | grep Geometry | awk '{print $2}' | cut -d'x' -f2)
+echo "Chromium Dimensions: Width=$WINDOW_WIDTH, Height=$WINDOW_HEIGHT" >> $logfile
 
 # Calculate the center of the Chromium window
 center_x=$((X_POS + WINDOW_WIDTH / 2))
 center_y=$((Y_POS + WINDOW_HEIGHT / 2))
+echo "Chromium Center Position: X=$center_x, Y=$center_y" >> $logfile
 
 # Get the end time (current time + 30 seconds)
 end_time=$(($(date +%s) + 30))
 
 while [[ $(date +%s) -lt $end_time ]]; do
-    # Define a range for randomness, e.g., 300 pixels in any direction
-    range=200
+    range=200  # Define a range for randomness, e.g., 300 pixels in any direction
 
     # Calculate random offsets from the center
     random_x=$((RANDOM % (range*2 + 1) - range))
@@ -42,8 +49,8 @@ while [[ $(date +%s) -lt $end_time ]]; do
     x=$((center_x + random_x))
     y=$((center_y + random_y))
 
-    # Log the coordinates to the terminal
-    echo "Moving mouse to: X=$x, Y=$y"
+    # Log the coordinates to the terminal and log file
+    echo "Moving mouse to: X=$x, Y=$y" | tee -a $logfile
 
     # Move the mouse to the computed location
     xdotool mousemove $x $y
@@ -51,5 +58,4 @@ while [[ $(date +%s) -lt $end_time ]]; do
     sleep 1  # Adjust this if you want more or fewer moves within that 30 seconds.
 done
 
-# Log the end time and a message to the file
 echo "Script finished at: $(date) and exited." >> $logfile
