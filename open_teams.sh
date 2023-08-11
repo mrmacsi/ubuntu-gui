107def94dd8caca2bdc0fd0af09b8e4745580647
MACIT SIMSEK <macitsimsek12@hotmail.com>
2023-08-11 18:14:33 +0100

    Update open_teams.sh

--- open_teams.sh
+++ open_teams.sh
@@ -3,12 +3,6 @@
 # Set the desired display
 export DISPLAY=:20
 
-# Start Xvfb on display :20
-Xvfb :20 -screen 0 1280x1024x24 &
-
-# Wait for Xvfb to start (a brief delay)
-sleep 1
-
 # Try to find the location of Chromium
 CHROMIUM_PATH=$(which chromium)
 if [ -z "$CHROMIUM_PATH" ]; then
@@ -20,6 +14,11 @@ CHROMIUM_ID=$(xdotool search --name "chromium" | awk 'NR==2')
 
 # If CHROMIUM_ID is empty, echo an error message and exit
 if [ -z "$CHROMIUM_ID" ]; then
+    # if Xvfb is not open start it
+    if ! pgrep -f "Xvfb :20"; then
+        Xvfb :20 -screen 0 1280x1024x24 &
+        sleep 1
+    fi
     # Open the URL using chromium-browser path
     $CHROMIUM_PATH 'https://teams.microsoft.com/_?culture=en-gb&country=gb#/conversations/48:notes?ctx=chat' &
 fi
