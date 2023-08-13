58cef601caf9a7e087d01a7671e74c100e4967c8
MACIT SIMSEK <macitsimsek12@hotmail.com>
2023-08-13 23:00:44 +0100

    Update open_teams.sh

--- open_teams.sh
+++ open_teams.sh
@@ -2,6 +2,7 @@
 
 # Set the desired display
 export DISPLAY=:20
+LARAVEL_PATH="/home/macit/work-server-screen/"
 
 # Try to find the location of Chromium
 CHROMIUM_PATH=$(which chromium)
@@ -19,6 +20,7 @@ if [ -z "$CHROMIUM_ID" ]; then
         Xvfb :20 -screen 0 1280x1024x24 &
         sleep 1
     fi
+    php "$LARAVEL_PATH"artisan set:status "on"
     # Open the URL using chromium-browser path
     $CHROMIUM_PATH 'https://teams.microsoft.com/_?culture=en-gb&country=gb#/conversations/48:notes?ctx=chat' &
 fi
