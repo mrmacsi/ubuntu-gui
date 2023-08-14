f4a2a93cfab4896353ec5d5895dfd41048f48617
MACIT SIMSEK <macitsimsek12@hotmail.com>
2023-08-14 18:43:09 +0100

    Update close_teams.sh

--- close_teams.sh
+++ close_teams.sh
@@ -7,7 +7,7 @@ php "$LARAVEL_PATH"artisan set:status "off"
 # Kill the Chromium process
 pkill -f chromium
 
-php "$LARAVEL_PATH"artisan add:activity "browswer_closed"
+php "$LARAVEL_PATH"artisan add:activity "browser_closed"
 
 echo "Teams on Chromium closed."
 
