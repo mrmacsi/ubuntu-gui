5f7fffadeaf69ed7eac445244a5948bb06e652d4
MACIT SIMSEK <macitsimsek12@hotmail.com>
2024-08-11 22:02:45 +0100

    Create close_browser.sh

---
+++ close_browser.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+LARAVEL_PATH="/home/macit/work-server-screen/"
+SCREENSHOT_FOLDER="screenshots/"
+SCREENSHOT_DIRECTORY="${LARAVEL_PATH}storage/${SCREENSHOT_FOLDER}"
+
+VARIABLES=$(php "$LARAVEL_PATH/artisan" variables)
+
+# Parse JSON using jq to get the FLIGHT_STATUS
+FLIGHT_STATUS=$(echo "$VARIABLES" | jq -r '.FLIGHT_STATUS')
+
+# Check if FLIGHT_STATUS is false
+if [ "$FLIGHT_STATUS" == "false" ]; then
+  # Kill the Chromium process
+  pkill -f chromium
+
+  # Log the activity
+  php "$LARAVEL_PATH/artisan" add:activity "browser_closed"
+
+  echo "Teams on Chromium closed."
+else
+  echo "Flight status is not false, skipping browser closure."
+fi
+
+# Exit the script
+exit 0
